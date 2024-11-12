2c8036946567008121c9498ce8970886cf9f18a3
AbuDaddy <abbamba06@gmail.com>
2024-11-12 16:40:01 +0100

    Add SQLAlchemy Database

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    db.init_app(app)
 
     return app
